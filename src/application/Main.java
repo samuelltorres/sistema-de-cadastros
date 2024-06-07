d751648e7969f32fb05233ca7d4cbd3747115a5f
Samuell Torres <samuelltorres145@gmail.com>
2024-06-07 18:31:28 -0300

    adicionado funcao de adicionar perguntas no arquivo formulario.txt e iniciado a funcao de deletar pergunta

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,11 +9,11 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    private static List<Person> list = new ArrayList<Person>();
+    private static List<Person> listPeoples = new ArrayList<Person>();
+    private static List<String> listFormQuestions = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
         int option;
 
         do {
@@ -30,21 +30,67 @@ public class Main {
                 case 2:
                     userList();
                     break;
+                case 3:
+                    formNewQuestion();
+                    break;
+                case 4:
+                    formDeleteQuestion();
+                    break;
+                case 6:
+                    break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
         } while (option != 6);
     }
 
+    public static void formNewQuestion() {
+        String pathForm = "D:\\ws-intellij\\registerSystem\\formulario.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathForm, true))) {
+            System.out.println("Digite a nova pergunta que será adicionada:");
+            String newFormQuestion = sc.nextLine();
+
+
+            int newIndex = listFormQuestions.size() + 5;
+            String newFormQuestionFormatted = String.format("%d - %s", newIndex, newFormQuestion);
+
+            listFormQuestions.add(newFormQuestion);
+
+            bw.newLine();
+            bw.write(newFormQuestionFormatted);
+
+            System.out.println("Pergunta adicionada com sucesso!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void formDeleteQuestion() {
+        String pathForm = "D:\\ws-intellij\\registerSystem\\formulario.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathForm, true))) {
+            System.out.println("Digite o numero da pergunta que será excluída:");
+            int index = sc.nextInt();
+            int indexFormated = index - 5;
+
+            listFormQuestions.remove(indexFormated);
+
+
+            System.out.println("Pergunta excluída com sucesso!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void userRegister() {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-
-
         String formPath = "d:\\ws-intellij\\registerSystem\\formulario.txt";
         File form = new File(formPath);
         String formFolder = form.getParent();
-
 
         try (BufferedReader br = new BufferedReader(new FileReader(form))) {
 
@@ -59,10 +105,10 @@ public class Main {
             int age = sc.nextInt();
             double height = sc.nextDouble();
 
-            list.add(new Person(name, email, age, height));
+            listPeoples.add(new Person(name, email, age, height));
 
-            for (int i = 0; i < list.size(); i++) {
-                Person p = list.get(i);
+            for (int i = 0; i < listPeoples.size(); i++) {
+                Person p = listPeoples.get(i);
                 String targetFileStr = formFolder + "\\" + (i + 1) + "-" + p.getNameFormated() + ".txt";
 
                 try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
@@ -84,11 +130,11 @@ public class Main {
     }
 
     public static void userList() {
-        if (list.isEmpty()) {
+        if (listPeoples.isEmpty()) {
             System.out.println("Nenhum usuário cadastrado.");
         } else {
-            for (int i = 0; i < list.size(); i++) {
-                Person p = list.get(i);
+            for (int i = 0; i < listPeoples.size(); i++) {
+                Person p = listPeoples.get(i);
                 System.out.println((i + 1) + " - " + p.getName());
             }
         }
@@ -100,5 +146,6 @@ public class Main {
         System.out.println("3 - Cadastrar nova pergunta no formulário");
         System.out.println("4 - Deletar pergunta do formulário");
         System.out.println("5 - Pesquisar usuário por nome ou idade ou email");
+        System.out.println("6 - Sair do menu");
     }
 }
